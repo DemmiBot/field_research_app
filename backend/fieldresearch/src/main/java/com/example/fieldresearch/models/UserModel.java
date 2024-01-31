20ec592062c234f92992730bc69c69cec578f421
Alleck Luka <allecklukap@gmail.com>
2024-01-30 21:04:20 -0400

    Conexao com db local pronta

---
+++ backend/fieldresearch/src/main/java/com/example/fieldresearch/models/UserModel.java
@@ -0,0 +1,48 @@
+package com.example.fieldresearch.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID user_id;
+    public UUID getUser_id() {
+        return user_id;
+    }
+    public void setUser_id(UUID user_id) {
+        this.user_id = user_id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public boolean isIs_admin() {
+        return is_admin;
+    }
+    public void setIs_admin(boolean is_admin) {
+        this.is_admin = is_admin;
+    }
+    private String name;
+    @Column(unique=true)
+    private String email;
+    private boolean is_admin;
+}
