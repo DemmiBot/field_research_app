80d90b7fbb843c12b1e43ba53f42a8927ce42c67
Gabriel Felix <102039571+gabrieelfelix@users.noreply.github.com>
2024-05-17 15:12:08 -0300

    feat: redesign login and signup screens (#14)
    * refactor: cubit implementation for users
    
    * fix: current user appearing in the list
    
    * feat: create icon and splash
    
    * refactor: delete user using cubit
    
    * fix: new body in signup
    
    * fix: mess in main app resolved
    
    * fix: bug in cubit resolved
    
    * refactor: delete obsolete folders
    
    * feat: create package ui
    
    * feat:Redesign login and signup screens
    
    * feat: redesign login and signup screens
    
    * fix: crazy git
    
    * refactor: some chanches

--- frontend/lib/app/app.dart
+++ frontend/lib/app/app.dart
@@ -1,11 +1,14 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:fieldresearch/screens/adm_page/create_form_view/bloc/create_form_bloc.dart';
 import 'package:fieldresearch/screens/adm_page/create_form_view/create_form_view.dart';
 import 'package:fieldresearch/screens/adm_page/home_adm_view/home_adm_view.dart';
 import 'package:fieldresearch/screens/adm_page/users_adm_view/users_adm_view.dart';
 import 'package:fieldresearch/screens/researcher_page/reseacher_home_view.dart';
 import 'package:fieldresearch/screens/sign_in_page/bloc/sign_in_bloc.dart';
-import 'package:fieldresearch/screens/sign_in_page/sign_in_view.dart';
 
-import 'package:fieldresearch/screens/sign_up_page/sign_up_view.dart';
+import 'package:fieldresearch/screens/sign_in_page/view/sign_in_view.dart';
+import 'package:fieldresearch/screens/sign_up_page/bloc/sign_up_bloc.dart';
+import 'package:fieldresearch/screens/sign_up_page/view/sign_up_view.dart';
 import 'package:fieldresearch/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -61,7 +64,7 @@ class MyAppView extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: 'Poppins',
-          scaffoldBackgroundColor: backGroundColor,
+          scaffoldBackgroundColor: MyColors.white,
         ),
         home: BlocBuilder<SignInBloc, ISignInState>(
           builder: (context, state) {
@@ -82,7 +85,7 @@ class MyAppView extends StatelessWidget {
 
             // failure login
             else {
-              return const SignupView();
+              return const SignInView();
             }
           },
         ),
---
+++ frontend/packages/app_ui/lib/src/constants.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+const kMargin = 32.0;
+
+class MyColors {
+  MyColors._();
+
+  static const Color transparent = Color(0x00ffffff);
+
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+
+  //divider
+  static const Color dividerColor = Color(0xffDCDCDC);
+
+  // buttons
+  static const Color primaryColor = Color(0xff217AFF);
+  static const Color secondaryButton = Color(0xff3F3F3F);
+
+  // TextFields
+  static const Color textFill = Color(0xffF3F3F3);
+  static const Color borderField = Color(0xffB8B8B8);
+}
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+
+// //Imagens do App
+// class MyAssetImages {
+//   MyAssetImages._();
+
+//   static const AssetImage homeBackground =
+//       AssetImage('assets/images/backgrounds/idp_norte_terreo_01.jpeg');
+// }
+
+// class MyColors {
+//   MyColors._();
+
+//   
+
+//  
+//   static const Color blue = Color(0xff0000ff);
+//   static const Color red = Color(0xffff0000);
+//   static const Color green = Color(0xff00ff00);
+//   static const Color lightBlue = Color(0xffaaaaff);
+
+//   static const Color primaryAccent = Color(0xff0884ff);
+
+//   static const Color primary = Color.fromRGBO(56, 34, 182, 1.0);
+//   static const Color secondary = Color(0xFF66BED3);
+//   // static const Color secondary = Color(0xff8ED0C7);
+//   static const Color grey = Color.fromRGBO(235, 235, 235, 1.0);
+//   static const Color dark = Color(0xff292929);
+
+//   // Buttons
+//   static const Color primaryButton = Color(0xff0884ff);
+//   static const Color secondaryButton = Color(0xff333335);
+
+//   // TextField
+//   static const Color textFill = Color(0xff333335);
+// }
+// import 'package:flutter/material.dart';
+
+// const lightColorScheme = ColorScheme(
+//   brightness: Brightness.light,
+//   primary: Color(0xFF1C5FA9),
+//   onPrimary: Color(0xFFFFFFFF),
+//   primaryContainer: Color(0xFFD5E3FF),
+//   onPrimaryContainer: Color(0xFFBA1A1A),
+//   secondary: Color(0xFF00629E),
+//   onSecondary: Color(0xFFFFFFFF),
+//   secondaryContainer: Color(0xFFCFE5FF),
+//   onSecondaryContainer: Color(0xFFBA1A1A),
+//   tertiary: Color(0xFF1C5FA9),
+//   onTertiary: Color(0xFFFFFFFF),
+//   tertiaryContainer: Color(0xFFD5E3FF),
+//   onTertiaryContainer: Color(0xFFBA1A1A),
+//   error: Color(0xFFBA1A1A),
+//   errorContainer: Color(0xFFFFDAD6),
+//   onError: Color(0xFFFFFFFF),
+//   onErrorContainer: Color(0xFF410002),
+//   background: Color(0xFFFDFBFF),
+//   onBackground: Color(0xFFBA1A1A),
+//   surface: Color(0xFFFDFBFF),
+//   onSurface: Color(0xFFBA1A1A),
+//   surfaceVariant: Color(0xFFE0E2EC),
+//   onSurfaceVariant: Color(0xFF43474E),
+//   outline: Color(0xFF74777F),
+//   onInverseSurface: Color(0xFFF1F0F4),
+//   inverseSurface: Color(0xFFBA1A1A),
+//   inversePrimary: Color(0xFFA6C8FF),
+//   shadow: Color(0xFFBA1A1A),
+//   surfaceTint: Color(0xFF1C5FA9),
+//   outlineVariant: Color(0xFFC4C6CF),
+//   scrim: Color(0xFFBA1A1A),
+// );
+
+// const darkColorScheme = ColorScheme(
+//   brightness: Brightness.dark,
+//   primary: Color(0xFFA6C8FF),
+//   onPrimary: Color(0xFF00305F),
+//   primaryContainer: Color(0xFF004787),
+//   onPrimaryContainer: Color(0xFFD5E3FF),
+//   secondary: Color(0xFF99CBFF),
+//   onSecondary: Color(0xFF003355),
+//   secondaryContainer: Color(0xFF004A78),
+//   onSecondaryContainer: Color(0xFFCFE5FF),
+//   tertiary: Color(0xFFA6C8FF),
+//   onTertiary: Color(0xFF00305F),
+//   tertiaryContainer: Color(0xFF004787),
+//   onTertiaryContainer: Color(0xFFD5E3FF),
+//   error: Color(0xFFFFB4AB),
+//   errorContainer: Color(0xFF93000A),
+//   onError: Color(0xFF690005),
+//   onErrorContainer: Color(0xFFFFDAD6),
+//   background: Color(0xFF1A1C1E),
+//   onBackground: Color(0xFFE3E2E6),
+//   surface: Color(0xFF1A1C1E),
+//   onSurface: Color(0xFFE3E2E6),
+//   surfaceVariant: Color(0xFF43474E),
+//   onSurfaceVariant: Color(0xFFC4C6CF),
+//   outline: Color(0xFF8D9199),
+//   onInverseSurface: Color(0xFF1A1C1E),
+//   inverseSurface: Color(0xFFE3E2E6),
+//   inversePrimary: Color(0xFF1C5FA9),
+//   shadow: Color(0xFF000000),
+//   surfaceTint: Color(0xFFA6C8FF),
+//   outlineVariant: Color(0xFF43474E),
+//   scrim: Color(0xFF000000),
+// );
