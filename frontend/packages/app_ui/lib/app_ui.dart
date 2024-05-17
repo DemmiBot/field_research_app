4387ac122bf9ae52c0313ec89dde82c4a1552158
Gabriel Felix <102039571+gabrieelfelix@users.noreply.github.com>
2024-05-17 12:20:54 -0300

    feat: redesign login and signup screens

--- frontend/packages/app_ui/lib/app_ui.dart
+++ frontend/packages/app_ui/lib/app_ui.dart
@@ -2,3 +2,5 @@ library app_ui;
 
 export 'src/constants.dart';
 export 'src/my_logo.dart';
+export 'src/custom_text_field.dart';
+export 'src/my_button.dart';
--- frontend/packages/app_ui/lib/src/my_logo.dart
+++ frontend/packages/app_ui/lib/src/my_logo.dart
@@ -1,28 +1,34 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyLogo extends StatelessWidget {
   const MyLogo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: const TextSpan(
-        text: 'SURVEY',
-        style: TextStyle(
-          color: MyColors.black,
-          fontSize: 40,
-          fontWeight: FontWeight.w900,
-        ),
-        children: <TextSpan>[
-          TextSpan(
-            text: 'APP',
-            style: TextStyle(
-                fontWeight: FontWeight.w900,
-                color: MyColors.primaryColor,
-                fontSize: 40),
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      child: RichText(
+        text: TextSpan(
+          text: 'SURVEY',
+          style: TextStyle(
+            color: MyColors.black,
+            fontSize: 40.sp,
+            fontWeight: FontWeight.w900,
           ),
-        ],
+          children: <TextSpan>[
+            TextSpan(
+              text: 'APP',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  color: MyColors.primaryColor,
+                  fontSize: 40.sp),
+            ),
+          ],
+        ),
       ),
     );
   }
